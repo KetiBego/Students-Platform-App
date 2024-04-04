59a4fad960c3a9a56b19f700c5f9d07311c7d195
KetiBego <kbegi20@freeuni.edu.ge>
2024-04-04 23:22:33 +0400

    Initial Spring Boot project setup

---
+++ backend/students-platform-app/src/main/java/ge/freeuni/studentsplatformapp/StudentsPlatformAppApplication.java
@@ -0,0 +1,13 @@
+package ge.freeuni.studentsplatformapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsPlatformAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsPlatformAppApplication.class, args);
+	}
+
+}
---
+++ backend/students-platform-app/src/test/java/ge/freeuni/studentsplatformapp/StudentsPlatformAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.freeuni.studentsplatformapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsPlatformAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
