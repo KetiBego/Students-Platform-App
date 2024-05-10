d2bcbf8426a34068094aa0b0e94ea81434dda3db
Ruska Keldishvili <rkeld20@freeuni.edu.ge>
2024-05-10 18:31:43 +0400

    ios project added

---
+++ Students-platform-app-ios/Students-platform-app-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Students-platform-app-ios
+//
+//  Created by Ruska Keldishvili on 10.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
