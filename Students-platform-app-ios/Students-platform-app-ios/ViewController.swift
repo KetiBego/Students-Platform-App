f082b5ce143dd2399451120bfed626d8fe142ac0
Ruska Keldishvili <rkeld20@freeuni.edu.ge>
2024-05-10 19:42:19 +0400

    ios networking module added and tested to work

--- Students-platform-app-ios/Students-platform-app-ios/ViewController.swift
+++ Students-platform-app-ios/Students-platform-app-ios/ViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import Networking
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tst = Test()
+        tst.printHello()
         // Do any additional setup after loading the view.
     }
 
